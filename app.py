7b96f88dc1da734186f155564d3c49ecd62e69c0
Anindyait <anindyakbiswas5@gmail.com>
2023-06-22 13:44:58 +0530

    Lots of cahnges
    New templates
    About
    CCTV
    Ability to capture and record
    Name display

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, Response, request
+import ctypes
+import os
+from flask import Flask, render_template, Response, request, redirect
 import face_recognition
 import numpy as np
 import cv2
@@ -6,38 +8,90 @@ import base64
 import io
 import PIL.Image
 import json
+import signal
+import threading
+from datetime import datetime
 
 app = Flask(__name__)
 
-'''
-keanu_img = face_recognition.load_image_file("./face_img/S1.jpg")
-keanu_encoding = face_recognition.face_encodings(keanu_img)[0]
 
-ryan_img = face_recognition.load_image_file("./face_img/S2.jpg")
-ryan_encoding = face_recognition.face_encodings(ryan_img)[0]
-
-p_img = face_recognition.load_image_file("./face_img/S3.png")
-p_encoding = face_recognition.face_encodings(p_img)[0]
-
-known_face_encodings = [keanu_encoding, ryan_encoding, p_encoding]
-known_face_names = ["Keanu Reeves", "Ryan Renolds", "Prateek"]
-
-
-data_dict = {}
-for i in range(len(known_face_names)):
-    data_dict[known_face_names[i]] = known_face_encodings[i]
-'''
 with open("data.json", "r") as file:
     data_dict = json.load(file)
 
 known_face_encodings = []
 known_face_names = []
 
+
+
 # Convert the values back to NumPy arrays in the dictionary
 for key, value in data_dict.items():
     data_dict[key] = np.array(value)
     known_face_encodings.append(value)
     known_face_names.append(key)
+    
+#recording settings
+
+print(os.path.dirname(__file__))
+fps = 30.0
+
+camera = cv2.VideoCapture(0)
+
+def cctv_capture_frame():
+    date_time  = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+    filename = os.path.join(os.path.dirname(__file__), 'static/captures/capture_' + date_time + '.png')
+    status, frame = camera.read()
+    frame = cv2.flip(frame, 1)
+    
+    cv2.imwrite(filename, frame)
+   
+def cctv_feed():
+    
+    while True:
+        # get camera frames
+        status, frame = camera.read()
+        frame = cv2.flip(frame, 1)
+        if not status:
+            break
+
+        ret, buff = cv2.imencode('.jpg', frame)
+        frame = buff.tobytes()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+def cctv_record():
+    date_time  = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+    filename = os.path.join(os.path.dirname(__file__), 'static/video/video_' + date_time + '.avi')
+    
+    print(os.path.dirname(__file__))
+
+   
+    frame_width = int(camera.get(3))
+    frame_height = int(camera.get(4))
+    
+    size = (frame_width, frame_height)
+    result = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'MJPG'), fps, size)
+
+    while True:
+        # get camera frames
+        global frame
+        status, frame = camera.read()
+        frame = cv2.flip(frame, 1)
+        if not status:
+            break
+       
+        result.write(frame)
+       
+       
+
+        #result.release()
+     
+# Create a thread for the infinite_loop function
+
+            
+            
+
+
 
 
 def video_feed():
@@ -49,6 +103,8 @@ def video_feed():
         # get camera frames
         status, frame = camera.read()
         frame = cv2.flip(frame, 1)
+        global name 
+        name= "-"
 
         if not status:
             break
@@ -58,33 +114,35 @@ def video_feed():
                 # Resize frame of video to 1/4 size for faster face recognition processing
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
                 # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                rgb_small_frame = small_frame
+                rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
                 # Only process every other frame of video to save time
 
                 # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(
-                    rgb_small_frame, model="hog")
-                face_encodings = face_recognition.face_encodings(
-                    rgb_small_frame, face_locations, model="small")
+                face_locations = face_recognition.face_locations(rgb_small_frame, model="hog")
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, known_face_locations=face_locations, model="small")
                 face_names = []
                 for face_encoding in face_encodings:
                     # See if the face is a match for the known face(s)
                     matches = face_recognition.compare_faces(
                         known_face_encodings, face_encoding)
-                    name = "Unknown"
+                    
                     # Or instead, use the known face with the smallest distance to the new face
                     face_distances = face_recognition.face_distance(
                         known_face_encodings, face_encoding)
                     best_match_index = np.argmin(face_distances)
                     # Threshold for similar looking people
-                    if face_distances[best_match_index] >= 0.54 :
-                        pass
+                    if face_distances[best_match_index] >= 0.55 :
+                        name="Unknown"
                     elif matches[best_match_index]:
                         name = known_face_names[best_match_index]
                         #print(name, " " , np.min(face_distances))
 
                     face_names.append(name)
+                    
+                    write_name()
+                    
+                    
             process_this_frame = (process_this_frame + 1) % 10
 
             # Display the results
@@ -121,6 +179,51 @@ def welcome():
 @app.route('/video')
 def video():
     return Response(video_feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# For cctv view
+@app.route('/cctv', methods=['GET', 'POST'])
+def cctv():
+    global thread
+    record_status = ' '
+    record_status= request.form.get('status')
+    print(record_status)
+
+     
+    if(record_status == 'true'):
+        thread.start()
+    else:
+        print('sdfgsdfsdf sdf sf sf')
+        try:
+            thread_id = thread.ident
+            if thread_id is not None:
+                thread_id = ctypes.c_long(thread_id)
+                ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, ctypes.py_object(KeyboardInterrupt))
+        except:
+            print("Exception")
+        finally:
+            thread = threading.Thread(target=cctv_record)         
+        
+    return render_template('cctv.html')
+
+
+@app.route('/cctv_capture', methods=['GET', 'POST'])
+def cctv_capture():
+    print("Capture")
+    cctv_capture_frame()
+    return Response("yo")
+
+@app.route('/cctv_view', methods=['GET', 'POST'])
+def cctv_view():
+    
+    cc = cctv_feed()
+    return Response(cc, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# For passing name
+@app.route('/write_name', methods=['POST'])
+def write_name():
+    print(name)
+
+    return Response(name)
 
 
 @app.route('/store_face_encoding', methods=['POST'])
@@ -159,6 +262,15 @@ def store_face_encoding():
         return "Face encoding stored successfully"
     else:
         return "No face detected"
+
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+@app.route('/about')
+def about():
+    return render_template('aboutus.html')
 
 
 if __name__ == '__main__':
